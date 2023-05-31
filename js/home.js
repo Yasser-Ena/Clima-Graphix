2f9c1fd344be6316f0932e956614a64132644b50
Yasser <yasseryoo05@gmail.com>
2023-05-31 09:02:56 +0100

    first-push

---
+++ js/home.js
@@ -0,0 +1,135 @@
+/**
+ * NAVBAR
+ */
+
+// Element variables
+const menu          = document.querySelector('.menu');
+const menuLines     = document.querySelectorAll('.line');
+const mobTabLinks   = document.querySelector('.mob-tab-links');
+const nav           = document.querySelector('nav');
+const section       = document.querySelector('section');
+const sideBar        = document.querySelector('#sidebar');
+
+
+// toggler function
+function togglerFunction(){
+
+    for (let i = 0; i < menuLines.length; i++) {
+        menuLines[i].classList.toggle('toggle');     
+    }
+
+    if(menu.classList.contains('toggle')){
+        menu.classList.remove('toggle');
+        mobTabLinks.style.display = "none";
+
+    }else{
+        menu.classList.add('toggle');
+        mobTabLinks.style.display = "flex";
+    }
+}
+
+// add the event listener to the menu icon
+menu.addEventListener("click", togglerFunction);
+
+// remove the menu links if the user click any wer outside the menu links it's self
+function removeTheLinks(){
+    for (let i = 0; i < menuLines.length; i++) {
+        menuLines[i].classList.remove('toggle');     
+    }
+    
+    menu.classList.remove('toggle');
+    mobTabLinks.style.display = "none";
+    
+};
+
+// add event listener to section and sidebar for remove the menu links using foreach lop
+[section, sideBar].forEach(item=>{
+    item.addEventListener('click', removeTheLinks);
+});
+
+/**
+ * MENU
+ */
+
+// Element variables
+const icons        = document.querySelector('.icons');
+const iconsItem    = document.querySelector('.icons-item');
+const notification = document.querySelector('.notification');
+const customize    = document.querySelector('.customize');
+const iconLinks    = document.querySelector('.icons-link');
+const links        = document.querySelector('.links');
+const body         = document.querySelector('body');
+
+// Variables
+const windowWidth = window.matchMedia('(min-width: 75rem');
+
+// Break the icons div to multiple element
+function breakDiv(){
+    
+
+    if(windowWidth.matches){
+
+    [menu, iconsItem].forEach(item=> {
+        icons.parentNode.appendChild(item);
+        item.style.display = "flex";
+    });
+
+    icons.remove();
+    
+    body.appendChild(iconLinks);
+    // links.classList.add('mob-tab-links');
+}
+}
+
+
+
+// Call breakDiv function when the page is loaded
+window.addEventListener('load', breakDiv);
+
+
+// Break the icons div to multiple element depend  on the with of the screen width
+// function breakDivDepend(){
+
+//     if(windowWidth.matches && nav.hasChildNodes(icons)){
+
+//     [menu, iconsItem].forEach(item=> {
+//         icons.parentNode.appendChild(item);
+//         item.style.display = "flex";
+//     });
+
+//     icons.remove();
+
+//     }else if(!nav.hasChildNodes(icons)){
+//         console.log('hi');
+
+//         const iconsSec = document.createElement('div');
+//         iconsSec.classList.add('icons');
+//         nav.appendChild(iconsSec);
+        
+        
+//         [menu, iconsItem].forEach(item=> {
+//             iconsSec.parentNode.appendChild(item);
+//             item.style.display = "flex";
+//         });
+    
+//     }
+
+// }
+
+// Call breakDivDepend function when the page is loaded
+// window.addEventListener('resize', breakDivDepend);
+
+/**
+ * MENU LINK's
+ */
+
+// Element Variables
+const searchBar = document.querySelector('.search-bar');
+
+// Call remove the links function that eexist in the MENU section for Close the menu links when the user click some were in the body items
+[section, sideBar, searchBar].forEach(item => {
+    item.addEventListener('click', removeTheLinks);
+});
+
+
+    
---
+++ style-guid.md
@@ -0,0 +1,104 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 360px
+- Tablet: 834px
+- Desktop: 1200px
+
+## Colors
+
+### Primary
+
+- Accent-Dark-Blue: #000956
+- Icons-Dark-Gray: #7C807F
+
+### Neutral
+
+- Background-Gray: #D9D9D9
+- Font-Light-Gray: #ECECEC
+
+## Typography
+
+### FONT Size
+
+#### Mobile
+- Extra-small: 10;
+- Extra-small thought’s: 10;
+- Extra-small-accent: 10;
+- H1-sub: 12;
+- H2-sub: 14;
+- Search-bar: 14;
+- H3-sub: 16;
+- logo: 18;
+- Medium-1: 18;
+- Medium-1-h3: 18;
+- Medium-2: 21;
+- H1: 24;
+- H2: 24;
+
+
+#### Tablet
+- Extra-small: 14;
+- Extra-small thought’s: 16;
+- H1-sub: 18;
+- H2-sub: 14;
+- Search-bar: 24;
+- H3-sub: 16;
+- logo: 18;
+- Medium-1: 30;
+- Medium-1-h3: 20;
+- Medium-2: 22;
+- H1: 34;
+- H2: 54;
+
+
+#### Desktop
+- Extra-small: 14;
+- Extra-small thought’s: 16;
+- H1-sub: 18;
+- H2-sub: 24;
+- Search-bar: 14;
+- H3-sub: 18;
+- logo: 18;
+- Medium-1: 30;
+- Medium-1-h3: 24;
+- Medium-2: 24;
+- H1: 34;
+- H2: 54;
+
+
+### Font Family
+
+- Family: [Nunito](https://fonts.google.com/specimen/Nunito)
+
+
+### Font Weight
+
+#### Mobile
+- Extra-small-weight: 200;
+- Search-bar-weight: 300;
+- H1-sub-weight: 300;
+- The-400: 400;
+- The-600: 600;
+- The-700: 700;
+
+
+#### Tablet
+- Extra-small-weight: 300;
+- Search-bar-weight: 300;
+- H1-sub-weight: 400;
+- The-400: 400;
+- The-600: 600;
+- The-700: 700;
+
+
+#### Desktop
+- Extra-small-weight: 300;
+- Search-bar-weight: 400;
+- H1-sub-weight: 400;
+- The-400: 400;
+- The-600: 600;
+- The-700: 700;
